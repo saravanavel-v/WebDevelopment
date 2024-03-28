20f90e2881361202247c774a5f7d32992c99cd55
Saravanavel <140529657+saravanavel-v@users.noreply.github.com>
2024-03-28 12:28:43 +0530

    Add files via upload
    commit after files added

---
+++ Portfolio/script.js
@@ -0,0 +1,28 @@
+
+
+// function setVideo(){
+//     let videos = document.querySelectorAll(".img > video");
+//     videos.forEach((video)=>{
+//         video.autoplay = true;
+//         video.loop = true;
+//         video.muted = true;
+
+
+//         video.addEventListener('error', function(e) {
+//             console.error('Error loading the video:', e);
+//         });
+        
+//         video.addEventListener('loadeddata', function() {
+//             console.log('Video loaded successfully');
+//             video.play().catch(function(error) {
+//                 console.error('Error playing the video:', error);
+//             });
+//         });
+
+
+//     })
+
+    
+// }
+
+// setVideo();
